f291f006ba39f53f7a9f492734759de7398ec688
Duy Quoc Nguyen <nqduy1999@gmail.com>
2025-01-17 20:10:43 +0700

    fix: update valid panarams

--- hashmap/valid-panaram.js
+++ hashmap/valid-panaram.js
@@ -8,17 +8,19 @@ An anagram is a string that contains the exact same characters as another string
 // hashmap here to check if
 const validPanagram = (s, t) => {
   let hashMapS = {};
+  if (s.length !== t.length) return false;
+
   for (let i = 0; i < s.length; i++) {
     hashMapS[s[i]] = (hashMapS[s[i]] ?? 0) + 1;
   }
-  for (let i = 0; i < s.length; i++) {
-    hashMapS[s[i]] = hashMapS[s[i]] - 1;
-
-    if (hashMapS[s[i]] < 0) {
+  for (let i = 0; i < t.length; i++) {
+    if (hashMapS[t[i]] === undefined) return false;
+    hashMapS[t[i]] = hashMapS[t[i]] - 1;
+    if (hashMapS[t[i]] < 0) {
       return false;
     }
   }
   return true;
 };
 
-console.log(validPanagram("racecar", "carrace"));
+console.log(validPanagram("aacc", "ccac"));
