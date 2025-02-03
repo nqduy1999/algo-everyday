493e36b414201efca15a9f721543a5ac154cb74d
Duy Nguyen <duy.se.it99@gmail.com>
2025-02-03 20:11:49 +0700

    feat: update stack challenge

--- greatfrontend/dsa/balanceBracket.js
+++ greatfrontend/dsa/balanceBracket.js
@@ -1,3 +1,29 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    return this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.length - 1];
+  }
+
+  get length() {
+    return this.stack.length;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
 function isBalancedBrackets(str) {
   const matchingBracket = {
     "{": "}",
@@ -5,12 +31,17 @@ function isBalancedBrackets(str) {
     "(": ")",
   };
 
-  const expectedMatches = str.length / 2;
-  const actualMatches = 0;
+  const stack = new Stack();
 
-  let hashMap
-
-  for (let i = 0; i < str.length; i++) {}
+  for (let i = 0; i <= str.length; i++) {
+    if (matchingBracket[stack.peek()] === str[i]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+  if (stack.length === 0) return true;
+  return false;
 }
 
 console.log(isBalancedBrackets(`([])`));
